2e8291aaae4c57c0801a2c26280c1e4409c393a4
Geoff Cox <slangingcode@gmail.com>
2020-10-15 01:37:19 -0400

    Chapter 2: The Basics - Finish

--- learning/src/app/app.component.ts
+++ learning/src/app/app.component.ts
@@ -3,13 +3,22 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  styles: [`
+  styleUrls: ['./app.component.css']
+  /* styles: [`
     h3 {
       color: dodgerblue;
     }
-  `]
+  `] */
 })
 export class AppComponent {
+  
+  username: string = '';
+  showSecret: boolean = false;
+  log: any[] = []
+
+  onToggleDetails(): void {
+    this.showSecret = !this.showSecret;
+    this.log.push(new Date());
+  }
 
 }
--- learning/src/app/servers/servers.component.ts
+++ learning/src/app/servers/servers.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server was created.';
+  serverName: string = 'Testserver';
+  serverCreated: boolean = false;
+  servers: string[] = ['Testserver', 'Testserver 2'];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(): void {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event): void {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
